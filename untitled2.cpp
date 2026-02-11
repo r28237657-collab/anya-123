6318b2afceefff72ebdd34f58a2a65cecde25949
r28237657-collab <r28237657@gmail.com>
2026-02-11 08:49:27 +0700

    add code

---
+++ untitled2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name ="rayuth"; //"Hello world"
+	int age = 15; // whole number
+	double price = 15.7; //fraction number
+	
+	cout << "My name is" << name;
+}
+
+
